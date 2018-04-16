23998b33d45600d7938319db463327cc2d695da4
Urbano Ceron <urban.rude@gmail.com>
2018-04-15 19:10:42 -0500

    Terminacion de curso TypeScript Udemy

--- app.ts
+++ app.ts
@@ -1,1 +1,29 @@
-console.log("Hello word with TypeScript");
+console.log("Hello.. word with TypeScript putitos");
+var myString:string = "String";
+console.log(myString);
+
+// tipo de datos
+var edad:number = 24;
+var altura:Number = new Number(1.80);
+
+var nombre:string = "Urbano Ceron";
+var apellido:String =new String ("Ceron");
+
+console.log("Mi nombre es "+nombre+" y mi edad es "+edad);
+//var p:Persona = new Persona();
+
+//funciones:tipoDeRetorno
+function sumar(num:number,numII:number):number{
+    return num+numII;
+}
+
+var Sumar = function(num:number,numII:number):number{
+    return num+numII;
+};
+
+var sumarNumeros = (num:number,numII:number) => {return num+numII};
+
+
+
+
+
